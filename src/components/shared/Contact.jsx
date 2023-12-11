e004f6550975263129a91ea4d401b968c2d84f0d
GhostMobin <s1810677110@ru.ac.bd>
2023-12-12 01:31:31 +0600

    contact 2

--- src/components/shared/Contact.jsx
+++ src/components/shared/Contact.jsx
@@ -1,30 +1,36 @@
 const Contact = () => {
     return (
         <div className="hero py-32 bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
-                <div className="card shrink-0 w-full max-w-xl border border-secondary bg-base-100">
+                <div className="card col-span-2 border border-secondary bg-base-100">
                     <form className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        <div className="grid grid-cols-2 gap-4">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered input-lg" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered input-lg" required />
+                            </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                            <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full" rows="6"></textarea>
+                            {/* <input type="password" placeholder="password" className="input input-bordered input-lg" required /> */}
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-secondary">Login</button>
+                            <button className="btn btn-secondary btn-lg">Login</button>
                         </div>
                     </form>
                 </div>
