9bb9a07ca7c0b13a4b1177c8ec39c06b9fcba015
GhostMobin <s1810677110@ru.ac.bd>
2023-12-12 15:58:21 +0600

    contact

--- src/components/shared/Contact.jsx
+++ src/components/shared/Contact.jsx
@@ -1,36 +1,72 @@
+import { PiPaperPlaneTiltFill, PiMapPinFill } from "react-icons/pi";
+import { IoMdMail, IoLogoWhatsapp } from "react-icons/io";
+
 const Contact = () => {
     return (
-        <div className="hero py-32 bg-base-200">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="hero">
+            <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="flex flex-col gap-12">
+                    <div className="flex items-center gap-4">
+                        <figure className="p-4 bg-secondary/40 rounded-md">
+                            <IoMdMail className="text-2xl" />
+                        </figure>
+                        <div>
+                            <h3 className="font-bold text-2xl">Email</h3>
+                            <p className="font-medium text-lg">shehjad0mobin@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center gap-4">
+                        <figure className="p-4 bg-secondary/40 rounded-md">
+                            <IoLogoWhatsapp className="text-2xl" />
+                        </figure>
+                        <div>
+                            <h3 className="font-bold text-2xl">Whatsapp</h3>
+                            <p className="font-medium text-lg">+88 017 3298 5103</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center gap-4">
+                        <figure className="p-4 bg-secondary/40 rounded-md">
+                            <PiMapPinFill className="text-2xl" />
+                        </figure>
+                        <div>
+                            <h3 className="font-bold text-2xl">Address</h3>
+                            <p className="font-medium text-lg">Rajshahi, Bangladesh</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="card col-span-2 border border-secondary bg-base-100">
                     <form className="card-body">
                         <div className="grid grid-cols-2 gap-4">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text">Name</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered input-lg" required />
+                                <input type="text" placeholder="Name" className="input input-bordered input-lg" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text">Email</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered input-lg" required />
+                                <input type="email" placeholder="Email" className="input input-bordered input-lg" required />
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Subject</span>
                             </label>
-                            <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full" rows="6"></textarea>
+                            <input type="text" placeholder="Subject" className="input input-bordered input-lg" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Massage</span>
+                            </label>
+                            <textarea placeholder="Message" className="textarea textarea-bordered textarea-lg w-full" rows="6"></textarea>
                             {/* <input type="password" placeholder="password" className="input input-bordered input-lg" required /> */}
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-secondary btn-lg">Login</button>
+                            <button className="btn btn-secondary btn-lg">Send <PiPaperPlaneTiltFill /></button>
                         </div>
                     </form>
                 </div>
