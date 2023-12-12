0877c6bb6668d7925c1a9bdfae1fa99bd5d32351
GhostMobin <s1810677110@ru.ac.bd>
2023-12-12 17:11:00 +0600

    polish about contact

--- src/components/shared/Contact.jsx
+++ src/components/shared/Contact.jsx
@@ -1,6 +1,7 @@
 import { PiPaperPlaneTiltFill, PiMapPinFill } from "react-icons/pi";
 import { IoMdMail, IoLogoWhatsapp } from "react-icons/io";
 import SectionTitle from "./SectionTitle";
+import Points from "./Points";
 
 const Contact = () => {
     return (
@@ -10,36 +11,11 @@ const Contact = () => {
             <div className="hero">
                 <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
                     <div className="flex flex-col gap-12">
-                        <div className="flex items-center gap-4">
-                            <figure className="p-4 bg-secondary/40 rounded-md">
-                                <IoMdMail className="text-2xl" />
-                            </figure>
-                            <div>
-                                <h3 className="font-bold text-2xl">Email</h3>
-                                <p className="font-medium text-lg">shehjad0mobin@gmail.com</p>
-                            </div>
-                        </div>
-
-                        <div className="flex items-center gap-4">
-                            <figure className="p-4 bg-secondary/40 rounded-md">
-                                <IoLogoWhatsapp className="text-2xl" />
-                            </figure>
-                            <div>
-                                <h3 className="font-bold text-2xl">Whatsapp</h3>
-                                <p className="font-medium text-lg">+88 017 3298 5103</p>
-                            </div>
-                        </div>
-
-                        <div className="flex items-center gap-4">
-                            <figure className="p-4 bg-secondary/40 rounded-md">
-                                <PiMapPinFill className="text-2xl" />
-                            </figure>
-                            <div>
-                                <h3 className="font-bold text-2xl">Address</h3>
-                                <p className="font-medium text-lg">Rajshahi, Bangladesh</p>
-                            </div>
-                        </div>
+                        <Points icon={IoMdMail} field='Email' value='shehjad0mobin@gmail.com' />
+                        <Points icon={IoLogoWhatsapp} field='Whatsapp' value='+88 017 3298 5103' />
+                        <Points icon={PiMapPinFill} field='Email' value='Rajshahi, Bangladesh' />
                     </div>
+
                     <div className="card col-span-2 border border-secondary bg-base-100">
                         <form className="card-body">
                             <div className="grid grid-cols-2 gap-4">
